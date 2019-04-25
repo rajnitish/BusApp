343c3ca0653d7fe520e12e43c152bf4f71f1aa17
rajnitish <raj.nitp@gmail.com>
2019-04-25 08:49:28 +0530

    Exit Activity added

---
+++ app/src/main/java/com/example/busapp/ExitActivity.java
@@ -0,0 +1,40 @@
+package com.example.busapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class ExitActivity extends Activity {
+
+    ImageView image;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exitlayout);
+
+        image = (ImageView) findViewById(R.id.imageView1);
+        image.setImageResource(R.drawable.exitimg);
+        int finishTime = 3; //10 secs
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, finishTime * 1000);
+
+
+    }
+
+    private void closeApp()
+    {
+        SystemClock.sleep(6000);
+        this.finishAffinity();
+        System.exit(1);
+    }
+
+}
+
