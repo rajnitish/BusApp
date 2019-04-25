995d3436268ff0c22c26aee5f2a7667c6f5f15b5
rajnitish <raj.nitp@gmail.com>
2019-04-25 11:06:02 +0530

    User Feedback susbsystem improvised.

--- app/src/main/java/com/example/busapp/ComplaintActivity.java
+++ app/src/main/java/com/example/busapp/ComplaintActivity.java
@@ -1,8 +1,10 @@
 package com.example.busapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
@@ -10,16 +12,23 @@ import android.widget.EditText;
 
 public class ComplaintActivity extends Activity {
     private EditText mEditTextSubject;
-    private EditText mEditTextMessage;
+    private EditText mNameText,mEmailText,mPhoneText,mFeedbackText;
+    ProgressDialog progressDialog;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.complaints_view);
-      //  mEditTextSubject=findViewById(R.id.edit_text_subject);
-      //  mEditTextMessage=findViewById(R.id.edit_text_message);
 
-         Button buttonSend = findViewById(R.id.buttonsend);
+        mNameText=findViewById(R.id.name_id);
+        mEmailText=findViewById(R.id.email_id);
+        mPhoneText=findViewById(R.id.phone_id);
+        mFeedbackText=findViewById(R.id.text_id);
+
+
+        Button buttonSend = findViewById(R.id.buttonsend);
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,17 +37,47 @@ public class ComplaintActivity extends Activity {
         });
     }
     private void sendMail(){
-//        String recipient = "anuj.avengerx@gmail.com";
+
         String[] recipientList=new String[]{"raj.nitp@gmail.com","anuj.avengerx@gmail.com","jaishreeram@gmail.com"};
-        String subject = "Customer_Complaint: "+mEditTextSubject.getText().toString();
-        String message = mEditTextMessage.getText().toString();
+        String subject = "Consumer Feedback/Complaint for BusApp V0.9";
+        String message = "Name: " + mNameText.getText().toString() + "\n Email Id" + mEmailText.getText().toString() + "\n Phone no: "
+                + mPhoneText.getText().toString() + "\n Feedback Text :" + mFeedbackText.getText().toString();
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.putExtra(Intent.EXTRA_EMAIL, recipientList);
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, message);
 
-        intent.setType("message/rfc822");
-        startActivity(Intent.createChooser(intent, " Choose your email Client"));
+        boolean flag = false;
+
+        if(mNameText.getText().toString().isEmpty() || mEmailText.getText().toString().isEmpty() || mPhoneText.getText().toString().isEmpty() || mFeedbackText.getText().toString().isEmpty())
+        {
+            progressDialog = new ProgressDialog(ComplaintActivity.this);
+            progressDialog.setMessage("Please fill all details ");
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+        else
+            flag = true;
+
+        if(flag) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_EMAIL, recipientList);
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(Intent.EXTRA_TEXT, message);
+
+            intent.setType("message/rfc822");
+            startActivity(intent);
+           // startActivity(Intent.createChooser(intent, " Choose your email Client"));
+
+            progressDialog = new ProgressDialog(ComplaintActivity.this);
+            progressDialog.setMessage("Thanks User for Feedback !!! ");
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+
+            int finishTime = 10; //10 secs
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    finish();
+                }
+            }, finishTime * 1000);
+        }
     }
 }
